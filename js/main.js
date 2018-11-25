3d710b5c786eede662147cb0d3b39cef72c49455
germanflores92 <41924910+germanflores92@users.noreply.github.com>
2018-11-25 14:22:40 -0500

    Added working files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* Google Maps */
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+        center: {lat:40.303501, lng:-73.993898},
+        zoom: 13
+    });
+} 
